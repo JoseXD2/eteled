bde327e5a6eddbf2699e324622c69e8fdb68c3ee
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-20 07:55:13 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -23,7 +23,9 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+#if newgrounds 
 import io.newgrounds.NG;
+#end
 import lime.app.Application;
 import openfl.Assets;
 
@@ -57,10 +59,10 @@ class TitleState extends MusicBeatState
 		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
 		#end*/
 		
-		#if sys
+		/*#if sys
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
+		#end*/
 
 		@:privateAccess
 		{
@@ -289,7 +291,7 @@ class TitleState extends MusicBeatState
 		#end
 
 		if (pressedEnter && !transitioning && skippedIntro)
-		{
+		{       #if newgrounds
 			#if !switch
 			NGio.unlockMedal(60960);
 
@@ -297,7 +299,7 @@ class TitleState extends MusicBeatState
 			if (Date.now().getDay() == 5)
 				NGio.unlockMedal(61034);
 			#end
-
+                        #end
 			if (FlxG.save.data.flashing)
 				titleText.animation.play('press');
 
